84eb0fa0d78876faf65d6ad86d415bdd30f6a933
dhavalgala <dhaval.gala59@gmail.com>
2016-01-27 18:52:54 +0530

    emergency json

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -178,6 +178,19 @@ angular.module('starter.controllers', ['ion-gallery', 'ngCordova'])
 
 .controller('EmergencyCtrl', function($scope, $ionicPopup, $timeout, MyServices, $state) {
 
+    $scope.notification = [{
+        image: "img/slider/3.jpg",
+        title: "Lorem ipsum dolor",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
+    }, {
+        image: "img/slider/4.jpg",
+        title: "Lorem ipsum dolor",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
+    }, {
+        image: "img/slider/1.jpg",
+        title: "Lorem ipsum dolor",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
+    }];
 
 })
 
