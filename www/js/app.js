fda9ac2920c529a188d03dcbb53adcd1daa0a151
Pooja <pthakare33@gmail.com>
2016-09-23 20:18:23 +0530

    Sert

--- www/js/app.js
+++ www/js/app.js
@@ -4,6 +4,88 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
+
+function onPushwooshInitialized(pushNotification) {
+
+    //if you need push token at a later time you can always get it from Pushwoosh plugin
+    pushNotification.getPushToken(
+        function(token) {
+            console.info('push token: ' + token);
+        }
+    );
+
+    //and HWID if you want to communicate with Pushwoosh API
+    pushNotification.getPushwooshHWID(
+        function(token) {
+            console.info('Pushwoosh HWID: ' + token);
+        }
+    );
+
+    //settings tags
+    pushNotification.setTags({
+            tagName: "tagValue",
+            intTagName: 10
+        },
+        function(status) {
+            console.info('setTags success: ' + JSON.stringify(status));
+        },
+        function(status) {
+            console.warn('setTags failed');
+        }
+    );
+
+    pushNotification.getTags(
+        function(status) {
+            console.info('getTags success: ' + JSON.stringify(status));
+        },
+        function(status) {
+            console.warn('getTags failed');
+        }
+    );
+
+    //start geo tracking.
+    //pushNotification.startLocationTracking();
+}
+
+function initPushwoosh() {
+    var pushNotification = cordova.require("pushwoosh-cordova-plugin.PushNotification");
+
+    //set push notifications handler
+    document.addEventListener('push-notification',
+        function(event) {
+            var message = event.notification.message;
+            var userData = event.notification.userdata;
+
+            alert("Push message opened: " + message);
+            console.info(JSON.stringify(event.notification));
+
+            //dump custom data to the console if it exists
+            if (typeof(userData) != "undefined") {
+                console.warn('user data: ' + JSON.stringify(userData));
+            }
+        }
+    );
+
+    //initialize Pushwoosh with projectid: "GOOGLE_PROJECT_ID", appid : "PUSHWOOSH_APP_ID". This will trigger all pending push notifications on start.
+    pushNotification.onDeviceReady({
+        projectid: "851657318083",
+        appid: "D24BF-777D4",
+        serviceName: ""
+    });
+
+    //register for push notifications
+    pushNotification.registerDevice(
+        function(status) {
+            alert("registered with token: " + status.pushToken);
+            onPushwooshInitialized(pushNotification);
+        },
+        function(status) {
+            alert("failed to register: " + status);
+            console.warn(JSON.stringify(['failed to register ', status]));
+        }
+    );
+}
+
 angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
 .run(function($ionicPlatform) {
@@ -20,44 +102,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
             StatusBar.backgroundColorByHexString("#c31727");
         }
 
-        try {
-            push = PushNotification.init({
-                "android": {
-                    "senderID": "134127417071",
-                    "icon": "icon",
-                    "vibrate": true
-                },
-                "ios": {
-                    "alert": "true",
-                    "badge": "true",
-                    "sound": "true"
-                },
-                "windows": {}
-            });
-
-            push.on('registration', function(data) {
-                console.log(data);
-                $.jStorage.set("device", data.registrationId);
-                var isIOS = ionic.Platform.isIOS();
-                var isAndroid = ionic.Platform.isAndroid();
-                if (isIOS) {
-                    $.jStorage.set("os", "iOS");
-                } else if (isAndroid) {
-                    $.jStorage.set("os", "Android");
-                }
-            });
-
-            push.on('notification', function(data) {
-                console.log(data);
-            });
-
-            push.on('error', function(e) {
-                console.log("ERROR");
-                console.log(e);
-            });
-        } catch (e) {
-            console.log(e)
-        }
+        initPushwoosh();
 
     });
 })
